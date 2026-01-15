fdae96b9dc598468246aa789d675d18de0745608
Damien <damien.tanguy@laophenixconsulting.com>
2026-01-15 05:31:44 +0100

    Modification du champ exports pour compatibilité Vite/Angular et du fichier rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -6,56 +6,25 @@ import json from "@rollup/plugin-json";
 import replace from "@rollup/plugin-replace";
 import postcss from "rollup-plugin-postcss";
 
-// import { babel } from "@rollup/plugin-babel";
-
-export default [
-  // browser-friendly UMD build
-  {
-    input: "src/index.js",
-    output: {
-      //   name: "epiviz.heatmap.gl",
-      //   file: pkg.browser,
-      dir: "dist",
-      format: "esm",
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      OMT(),
-      json(),
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("production"),
-        preventAssignment: true,
-      }),
-      postcss({
-        extract: "ehgl.css",
-        minimize: true,
-      }),
-    ],
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/epiviz.heatmap.gl.js", // On fixe le nom du fichier
+    format: "esm",                    // Format ESM obligatoire pour Vite/Angular
+    sourcemap: true,
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: "src/index.js",
-    external: ["epiviz.gl"],
-    output: [
-      { file: pkg.main, format: "cjs" },
-      //   { file: pkg.module, format: "es" },
-    ],
-    plugins: [
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("production"),
-        preventAssignment: true,
-      }),
-      postcss({
-        extract: "ehgl.css",
-        minimize: true,
-      }),
-    ],
-  },
-];
+  plugins: [
+    resolve(),
+    commonjs(),
+    OMT(),
+    json(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      preventAssignment: true,
+    }),
+    postcss({
+      extract: "ehgl.css",
+      minimize: true,
+    }),
+  ],
+};
